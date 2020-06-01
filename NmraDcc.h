a09c6b10028f23050bed6c1f1ef12fae5ece77c5
Alex Shepherd <kiwi64ajs@gmail.com>
2020-06-02 09:50:12 +1200

    bumped NMRADCC_VERSION to 205

--- NmraDcc.h
+++ NmraDcc.h
@@ -57,7 +57,7 @@
 #ifndef NMRADCC_IS_IN
 #define NMRADCC_IS_IN
 
-#define NMRADCC_VERSION     203     // Version 2.0.3
+#define NMRADCC_VERSION     205     // Version 2.0.5
 
 #define MAX_DCC_MESSAGE_LEN 6    // including XOR-Byte
 
